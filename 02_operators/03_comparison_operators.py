149df2abdf00871028d7c4d69861f6989fdb9667
Manas Sahu <168023535+Manassahu01@users.noreply.github.com>
2026-07-08 23:57:54 +0530

    operator in python

---
+++ 02_operators/03_comparison_operators.py
@@ -0,0 +1,120 @@
+"""
+=========================================================
+Topic       : Comparison Operators in Python
+Author      : Manas Sahu
+Repository  : python-practice
+=========================================================
+
+Theory:
+--------
+Comparison operators compare two values and return
+either True or False.
+
+Operators:
+
+==   Equal To
+!=   Not Equal To
+>    Greater Than
+<    Less Than
+>=   Greater Than or Equal To
+<=   Less Than or Equal To
+"""
+
+print("=" * 60)
+print("COMPARISON OPERATORS")
+print("=" * 60)
+
+a = 20
+b = 10
+
+print("a =", a)
+print("b =", b)
+
+print()
+
+# Equal To
+print("Equal To (==)")
+print(a == b)
+
+print()
+
+# Not Equal To
+print("Not Equal To (!=)")
+print(a != b)
+
+print()
+
+# Greater Than
+print("Greater Than (>)")
+print(a > b)
+
+print()
+
+# Less Than
+print("Less Than (<)")
+print(a < b)
+
+print()
+
+# Greater Than or Equal To
+print("Greater Than or Equal To (>=)")
+print(a >= b)
+
+print()
+
+# Less Than or Equal To
+print("Less Than or Equal To (<=)")
+print(a <= b)
+
+print()
+
+print("=" * 60)
+print("MORE EXAMPLES")
+print("=" * 60)
+
+x = 50
+y = 50
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+print()
+
+name1 = "Python"
+name2 = "Python"
+
+print(name1 == name2)
+
+print()
+
+age = 18
+
+print(age >= 18)
+
+print()
+
+marks = 35
+
+print(marks >= 33)
+
+print()
+
+salary = 50000
+
+print(salary > 30000)
+
+print()
+
+temperature = 25
+
+print(temperature < 30)
+
+print()
+
+print("=" * 60)
+print("END OF COMPARISON OPERATORS")
+print("=" * 60)
